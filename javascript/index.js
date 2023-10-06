cf0ff924e5faba9f16bd70ef5a59e884ab28213e
Goekhan Agkurt <g.agkurt@hotmail.de>
2023-10-06 12:30:59 +0200

    add bookmark and show answer function

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,20 @@
 // phew… not a lot going on here. Please add some code!
+
+const buttonAnswer = document.querySelector('[data-js="button-answer"]');
+const answer = document.querySelector('[data-js="card-answer"]');
+const bookmark = document.querySelector('[data-js="bookmark"]');
+
+
+
+bookmark.addEventListener("click", () => {
+    bookmark.classList.toggle("bookmark--active");
+});
+
+
+buttonAnswer.addEventListener("click", () => {
+    answer.classList.toggle("card__answer");
+    
+})
+
+
+
